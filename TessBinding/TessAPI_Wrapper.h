ffe28510441ee8798c61915a38eed41df27758bc
profPlum <dwyer_deighan@brown.edu>
2016-08-16 16:18:42 -0400

    TessBinding initial commit

---
+++ TessBinding/TessAPI_Wrapper.h
@@ -0,0 +1,156 @@
+#ifndef TESSAPI_WRAPPER_H
+#define TESSAPI_WRAPPER_H
+
+// fixes an annoying macro issue on windows
+#define NOMINMAX
+
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+#include <Eigen/Core>
+
+#include <string>
+#include <list>
+#include <tuple>
+#include <memory>
+#include <stdexcept>
+
+enum FontAttribute
+{
+	PLAIN,
+	BOLD,
+	ITALIC,
+	UNDER_LINED
+};
+
+struct FontAttrLabel
+{
+	FontAttrLabel(FontAttribute attr, uint16_t start, uint16_t length) :
+		attr(attr),
+		start(start),
+		length(length)
+	{ }
+
+	FontAttribute attr = PLAIN;
+	uint16_t start; // inclusive
+	uint16_t length;
+};
+
+// wrapper class for the tesseract api that only exposes necessary functionality
+// and in a memory managed way
+class TessAPI_Wrapper : public tesseract::TessBaseAPI
+{
+private:
+	// holds image data until it is no longer needed
+	bool recognizeNeeded = false;
+	Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic> img_data;
+public:
+	TessAPI_Wrapper() :
+		tesseract::TessBaseAPI()
+	{
+		if (this->Init(NULL, "eng"))
+		{
+			throw std::runtime_error("initialization of internal 'TessBaseAPI()' class failed");
+		}
+	}
+	~TessAPI_Wrapper()
+	{ }
+
+	// pybind11 can't convey inheritance unless we essentially redefine the parent class, this is easier
+	int MeanTextConf_wrap() { return this->MeanTextConf(); }
+	void SetRectangle_wrap(int x, int y, int w, int h) { return this->SetRectangle(x, y, w, h); }
+	bool SetVariable_wrap(std::string var, std::string value) { return this->SetVariable(var.c_str(), value.c_str()); }
+	void ReadConfigFile_wrap(std::string file_name) { this->ReadConfigFile(file_name.c_str()); }
+
+	void SetImage_cv2(Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic> img)
+	{
+		// move data to class member to postpone deletion
+		img_data = std::move(img);
+		img_data.transposeInPlace();
+
+		// flatten the image data
+		Eigen::Map< Eigen::Matrix<uint8_t, Eigen::Dynamic, 1> > flat(img_data.data(), img_data.size());
+
+		// use rows in place and of columns and columns in place of rows because we transposed the data
+		this->SetImage(flat.data(), img_data.rows(), img_data.cols(), 1, img_data.rows());
+	}
+
+	std::tuple<std::string, std::list<FontAttrLabel>> GetTextWithAttrs()
+	{
+		this->Recognize(0);
+		
+		std::unique_ptr<tesseract::ResultIterator> itr(this->GetIterator());
+		std::list<FontAttrLabel> textAttrGroups;
+
+		std::string fullText;
+		FontAttribute groupAttr;
+		uint16_t groupStart = 0;
+		uint16_t groupSize = 0;
+
+		bool firstGo = true;
+
+		// iterate through OCRed words
+		do
+		{
+			// we only care about isBold, isItalic, and isUnderLined
+			bool isBold, isItalic, isUnderLined, isMonoSpace, isSerif, isSmallCaps;
+			int pointsize, fond_id;
+
+			itr->WordFontAttributes(&isBold, &isItalic, &isUnderLined, &isMonoSpace, &isSerif, &isSmallCaps, &pointsize, &fond_id);
+
+			FontAttribute txtAttr = isBold ? BOLD : isItalic ? ITALIC : isUnderLined ? UNDER_LINED : PLAIN;
+
+			std::unique_ptr<char[]> text(itr->GetUTF8Text(tesseract::RIL_WORD));
+
+			if (firstGo)
+			{
+				groupAttr = txtAttr;
+				firstGo = false;
+			}
+			
+			if (txtAttr != groupAttr) // end this group and start a new one
+			{
+				textAttrGroups.push_back(FontAttrLabel(groupAttr, groupStart, groupSize));
+
+				groupStart += groupSize;
+				groupSize = 0;
+				groupAttr = txtAttr;
+			}
+			
+			fullText += text.get();
+			groupSize += (uint16_t)strlen(text.get());
+
+			// if not the last word of the entire block of text
+			if (!itr->IsAtFinalElement(tesseract::RIL_BLOCK, tesseract::RIL_WORD))
+			{
+				// add appropriate whitespace (" " or "\n") depending on whether this is the end of a line or not
+				fullText += itr->IsAtFinalElement(tesseract::RIL_TEXTLINE, tesseract::RIL_WORD) ? "\n" : " ";
+				groupSize++;
+			}
+
+		} while (itr->Next(tesseract::RIL_WORD));
+
+		// the last group will not be appended in the loop because the text will end before the group does
+		textAttrGroups.push_back(FontAttrLabel(groupAttr, groupStart, groupSize));
+		
+		return std::tuple<std::string, std::list<FontAttrLabel>>(fullText, textAttrGroups);
+	}
+
+	std::tuple<int, int> TotalConfidence()
+	{
+		std::unique_ptr<int[]> confidence_list(this->AllWordConfidences());
+		int* confidence_iter = confidence_list.get();
+
+		int total_confidence = 0;
+		int num_words = 0;
+
+		while (*confidence_iter != -1)
+		{
+			total_confidence += *(confidence_iter++);
+			num_words++;
+		}
+
+		return std::tuple<int, int>(total_confidence, num_words);
+	}
+};
+
+#endif
---
+++ TessBinding/TessBinding.cpp
@@ -0,0 +1,64 @@
+// TessBinding.cpp : Defines the entry point for the console application.
+//
+
+#include "TessAPI_Wrapper.h"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
+
+namespace py = pybind11;
+
+PYBIND11_PLUGIN(TessBinding) {
+	py::module m("TessBinding", "tesseract api bindings DD");
+
+	/*py::enum_<tesseract::PageSegMode>(m, "PageSegMode")
+		.value("PSM_OSD_ONLY", tesseract::PSM_OSD_ONLY)
+		.value("PSM_AUTO_OSD", tesseract::PSM_AUTO_OSD)
+		.value("PSM_AUTO_ONLY", tesseract::PSM_AUTO_ONLY)
+		.value("PSM_SINGLE_COLUMN", tesseract::PSM_SINGLE_COLUMN)
+		.value("PSM_SINGLE_BLOCK_VERT_TEXT", tesseract::PSM_SINGLE_BLOCK_VERT_TEXT)
+		.value("PSM_SINGLE_BLOCK", tesseract::PSM_SINGLE_BLOCK)
+		.value("PSM_SINGLE_LINE", tesseract::PSM_SINGLE_LINE)
+		.value("PSM_SINGLE_WORD", tesseract::PSM_SINGLE_WORD)
+		.value("PSM_CIRCLE_WORD", tesseract::PSM_CIRCLE_WORD)
+		.value("PSM_SINGLE_CHAR", tesseract::PSM_SINGLE_CHAR)
+		.value("PSM_SPARSE_TEXT", tesseract::PSM_SPARSE_TEXT)
+		.value("PSM_SPARSE_TEXT_OSD", tesseract::PSM_SPARSE_TEXT_OSD)
+		.value("PSM_RAW_LINE", tesseract::PSM_RAW_LINE)
+		.export_values();*/
+
+	py::enum_<FontAttribute>(m, "FontAttribute")
+		.value("PLAIN", FontAttribute::PLAIN)
+		.value("BOLD", FontAttribute::BOLD)
+		.value("ITALIC", FontAttribute::ITALIC)
+		.value("UNDER_LINED", FontAttribute::UNDER_LINED)
+		.export_values();
+	
+	py::class_<FontAttrLabel>(m, "FontAttrLabel")
+		.def(py::init<FontAttribute, uint16_t, uint16_t>())
+		.def_readwrite("attr", &FontAttrLabel::attr)
+		.def_readwrite("start", &FontAttrLabel::start)
+		.def_readwrite("length", &FontAttrLabel::length);
+
+	//TODO: bind some functions for getting tesseract config values
+	py::class_<TessAPI_Wrapper>(m, "TessBaseAPI")
+		.def(py::init<>())
+		.def("SetVariable", &TessAPI_Wrapper::SetVariable_wrap, "Set a config variable for tesseract, "
+			"returns false if name lookup failed. "
+			"Notable variables include 'tessedit_pageseg_mode' and 'tessedit_char_whitelist'.")
+		.def("SetImage", &TessAPI_Wrapper::SetImage_cv2, "Set the image tesseract will work with. (uses opencv2-numpy image)")
+		.def("ReadConfigFile", &TessAPI_Wrapper::ReadConfigFile_wrap, "Read-in a config file.")
+		.def("MeanTextConf", &TessAPI_Wrapper::MeanTextConf_wrap, "Mean confidence of all words recognized on this image.")
+		.def("TotalConfidence", &TessAPI_Wrapper::TotalConfidence, "Returns a tuple of sum of confidence scores and number of words i.e. (total_confidence, num_words)."
+			" Use this for getting a mean confidence over multiple images.")
+		.def("SetRectangle", &TessAPI_Wrapper::SetRectangle_wrap, "Set the bounding rectangle tesseract will use when reading text."
+			" ((x,y) is the top left corner of that box)",
+			py::arg("x"), py::arg("y"), py::arg("w"), py::arg("h"))
+		.def("GetTextWithAttrs", &TessAPI_Wrapper::GetTextWithAttrs, "Gets OCRed text with font attribute information (i.e. categorized as PLAIN, BOLD, ITALIC, or UNDER_LINED)\n"
+																	 "Return value is a tuple with the OCRed text and a list of FontAttrLabels with start, length and attr members");
+	   //.def("SetPageSegMode", &TessAPI_Wrapper::SetPageSegMode, "Set the page segment mode.")
+	   //.def("GetPageSegMode", &TessAPI_Wrapper::GetPageSegMode, "Get the page segment mode.");
+
+	return m.ptr();
+}
